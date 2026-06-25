a316c2f1b9b760edd12b4c8badb64d647529c0fb
Otaxonov <Otaxonov_98@mail.ru>
2026-06-25 22:59:12 +0500

    Python

--- main.py
+++
@@ -1,9 +0,0 @@
-from math import pi, e, cos, sin, tan, log, sqrt
-
-r1, r2, r3 = map(int, input().split())
-
-s1 = pi * pow(r1, 2)
-s2 = pi * pow(r2, 2)
-s3 = pi * pow(r3, 2)
-
-print(f"{s1:.2f} {s2:.2f} {s3:.2f}")
