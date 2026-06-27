95e80102f158de6de5a1c61d8d100e0270efe2ed
Otaxonov <Otaxonov_98@mail.ru>
2026-06-27 23:47:46 +0500

    Python

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from math import sin
+from math import log
 
-n = int(input())
+a, b, c, d = map(int, input().split())
 
 s = 0
-for i in range(1, n + 1):
-    s += sin(i) / pow(2, i)
+for m in range(1, a + 1):
+    A = 3 * pow(m, 3) + 4 * m + 5
+    B = pow(m, 3) + log(m)
+    s += A / B
 
-print(f"{s:.2f}")
+p = 1
+for k in range(1, b + 1):
+    p *= k / (pow(k, 3) + 7 * k + 5)
+
+# Nested for loop
+sp = 0
+for i in range(1, c + 1):
+    p1 = 1
+    for m in range(1, d + 1):
+        p1 *= (log(i) + pow(m, i)) / pow(m, i)
+    sp += p1
+
+print(f"{s:.2f} {p:.2f} {sp:.2f}")
